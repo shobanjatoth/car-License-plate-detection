efef4edde43dc6de2724bd5be997eb8ad5d6f23a
shobanjatoth <146610402+shobanjatoth@users.noreply.github.com>
2025-04-06 13:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,6 @@ def upload_file():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
+    
